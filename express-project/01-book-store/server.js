32a2eae659165b9fec71ad602b2fa93d00bc085f
gehlot0709 <gehlotstudy@gmail.com>
2026-01-03 11:18:59 +0530

    Add

--- express-project/01-book-store/server.js
+++ express-project/01-book-store/server.js
@@ -11,7 +11,7 @@ app.post("/addData", async (req, res) => {
 })
 app.get("/", async (req, res) => {
     const data = await bookStore.find({})
-    return res.render(data)
+    return res.send(data)
 })
 app.listen(8000, () => {
     console.log("server start with 8000")
